a423ec8d346bdd81380175dd8cdf70fce78f3f3f
ngangavic <ngangavictor10@gmail.com>
2020-08-19 20:24:01 +0300

    details activity UI

---
+++ app/src/main/java/com/ngangavictor/firestore/register/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.ngangavictor.firestore.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ngangavictor.firestore.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
