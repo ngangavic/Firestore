ed4e5d4c8415688db3af75b915af8fd233a198d9
ngangavic <ngangavictor10@gmail.com>
2020-08-19 20:36:40 +0300

    navigation drawer

---
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/exams/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.ngangavictor.firestore.school.ui.exams
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
