be53afbf640a87af57c7612a3ec4ab9fc6787253
ngangavic <ngangavictor10@gmail.com>
2020-08-20 18:03:10 +0300

    add classes

--- app/src/main/java/com/ngangavictor/firestore/school/ui/classes/ClassFragment.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/classes/ClassFragment.kt
@@ -1,18 +1,38 @@
 package com.ngangavictor.firestore.school.ui.classes
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.EditText
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.ngangavictor.firestore.R
 
 class ClassFragment : Fragment() {
 
     private lateinit var classViewModel: ClassViewModel
+
+    private lateinit var root: View
+
+    private lateinit var alert: AlertDialog
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseFirestore
+
+    private lateinit var recyclerViewClasses: RecyclerView
+    private lateinit var fabAddClass: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,11 +41,64 @@ class ClassFragment : Fragment() {
     ): View? {
         classViewModel =
             ViewModelProviders.of(this).get(ClassViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_class, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        classViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        root = inflater.inflate(R.layout.fragment_class, container, false)
+
+        auth = Firebase.auth
+        database = Firebase.firestore
+
+        recyclerViewClasses = root.findViewById(R.id.recyclerViewClasses)
+        fabAddClass = root.findViewById(R.id.fabAddClass)
+
+        fabAddClass.setOnClickListener {
+            addClassAlert()
+        }
+
         return root
     }
+
+
+    private fun addClassAlert() {
+        val editTextClassName = EditText(requireContext())
+        editTextClassName.hint = "Enter class name"
+
+        val classAlert = AlertDialog.Builder(requireContext())
+        classAlert.setCancelable(false)
+            .setTitle("Add Class")
+            .setView(editTextClassName)
+            .setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+            .setPositiveButton("Ok") { _, _ ->
+                if (TextUtils.isEmpty(editTextClassName.text.toString())) {
+                    editTextClassName.requestFocus()
+                    editTextClassName.error = "Cannot be empty"
+                } else {
+                    addClass(editTextClassName.text.toString())
+                }
+            }
+
+        alert = classAlert.create()
+        alert.show()
+    }
+
+    private fun addClass(className: String) {
+
+        val data = hashMapOf(
+            className.replace(" ","") to className
+        )
+
+        database.collection("classes").document(auth.currentUser!!.uid)
+            .set(data,SetOptions.merge()).addOnCompleteListener {
+                alert.cancel()
+                if (it.isSuccessful) {
+                    Snackbar.make(requireView(), "Class added", Snackbar.LENGTH_LONG).show()
+                } else {
+                    Snackbar.make(
+                        requireView(),
+                        "Error while adding class. Try again",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
 }
