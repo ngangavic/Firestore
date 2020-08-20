e80a432601ecebf12ea3b7271491803628158e69
ngangavic <ngangavictor10@gmail.com>
2020-08-20 20:40:22 +0300

    class adapter

---
+++ app/src/main/java/com/ngangavictor/firestore/adapter/ClassAdapter.kt
@@ -0,0 +1,30 @@
+package com.ngangavictor.firestore.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ngangavictor.firestore.R
+import com.ngangavictor.firestore.holder.ClassHolder
+import com.ngangavictor.firestore.models.ClassModel
+
+class ClassAdapter(private val classes: ArrayList<ClassModel>): RecyclerView.Adapter<ClassHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClassHolder {
+        val viewHolder: ClassHolder?
+        val layoutView =
+            LayoutInflater.from(parent.context).inflate(R.layout.row_class, parent, false)
+        viewHolder = ClassHolder(layoutView)
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ClassHolder, position: Int) {
+        holder.textViewName.text=classes[position].className
+        holder.imageViewMore.setOnClickListener {
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return classes.size
+    }
+}
