9dd5d045c0833bb3295b8d05c2d0f61bb281fa6d
ngangavic <ngangavictor10@gmail.com>
2020-08-23 05:52:59 +0300

    fragment subject setup

--- app/src/main/java/com/ngangavictor/firestore/school/ui/subjects/SubjectFragment.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/subjects/SubjectFragment.kt
@@ -8,11 +8,21 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.ngangavictor.firestore.R
 
 class SubjectFragment : Fragment() {
 
     private lateinit var subjectViewModel: SubjectViewModel
+
+    private lateinit var root:View
+
+    private lateinit var recyclerViewSubjects:RecyclerView
+
+    private lateinit var fabAddSubject:FloatingActionButton
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,10 +31,19 @@ class SubjectFragment : Fragment() {
     ): View? {
         subjectViewModel =
             ViewModelProviders.of(this).get(SubjectViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_subject, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
+
+        root = inflater.inflate(R.layout.fragment_subject, container, false)
+
+        recyclerViewSubjects=root.findViewById(R.id.recyclerViewSubjects)
+
+        fabAddSubject=root.findViewById(R.id.fabAddSubject)
+
+        recyclerViewSubjects.layoutManager =
+            LinearLayoutManager(requireContext())
+        recyclerViewSubjects.setHasFixedSize(true)
+
         subjectViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
         })
         return root
     }
