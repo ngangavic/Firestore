6bd440edc3fe2ba7a6fd4faf9d45cbf58690e61c
ngangavic <ngangavictor10@gmail.com>
2020-08-23 05:07:46 +0300

    logout

--- app/src/main/java/com/ngangavictor/firestore/school/SchoolActivity.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/SchoolActivity.kt
@@ -1,7 +1,9 @@
 package com.ngangavictor.firestore.school
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,11 +13,20 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ngangavictor.firestore.R
+import com.ngangavictor.firestore.login.LoginActivity
+import com.ngangavictor.firestore.utils.LocalSharedPreferences
 
 class SchoolActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var localSharedPreferences: LocalSharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,12 +48,29 @@ class SchoolActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        auth=Firebase.auth
+
+        localSharedPreferences= LocalSharedPreferences(this@SchoolActivity)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.school, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_logout->{
+                auth.signOut()
+                localSharedPreferences.clearSchoolDetailsPref()
+                startActivity(Intent(this@SchoolActivity,LoginActivity::class.java))
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
