5b4860e0b42b53e42e99fa9e5f527f5340c26357
ngangavic <ngangavictor10@gmail.com>
2020-08-23 21:41:29 +0300

    subject menu

--- app/src/main/java/com/ngangavictor/firestore/adapter/SubjectAdapter.kt
+++ app/src/main/java/com/ngangavictor/firestore/adapter/SubjectAdapter.kt
@@ -2,13 +2,12 @@ package com.ngangavictor.firestore.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.ngangavictor.firestore.R
-import com.ngangavictor.firestore.holder.ExamHolder
 import com.ngangavictor.firestore.holder.SubjectHolder
-import com.ngangavictor.firestore.listeners.ListenerDeleteExam
-import com.ngangavictor.firestore.models.ExamModel
 import com.ngangavictor.firestore.models.SubjectModel
 
 class SubjectAdapter(private val context: Context, private val subjects: ArrayList<SubjectModel>) :
@@ -23,9 +22,28 @@ class SubjectAdapter(private val context: Context, private val subjects: ArrayLi
     }
 
     override fun onBindViewHolder(holder: SubjectHolder, position: Int) {
-        holder.textViewName.text=subjects[position].subjectName
+        holder.textViewName.text = subjects[position].subjectName
         holder.imageViewMore.setOnClickListener {
+            val popupMenu = PopupMenu(context, holder.imageViewMore)
+            popupMenu.inflate(R.menu.subject_menu)
+            popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                override fun onMenuItemClick(item: MenuItem?): Boolean {
+                    when (item!!.itemId) {
+                        R.id.action_edit_subject -> {
+                            return true
+                        }
+                        R.id.action_assign_class_subject -> {
+                            return true
+                        }
+                        R.id.action_delete_subject -> {
+                            return true
+                        }
+                    }
+                    return false
+                }
 
+            })
+            popupMenu.show()
         }
     }
 
