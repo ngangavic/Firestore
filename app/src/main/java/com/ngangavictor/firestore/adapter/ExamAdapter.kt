364ae10c136dee6050fe332014e23baded13660c
ngangavic <ngangavictor10@gmail.com>
2020-08-23 05:13:38 +0300

    exam menu

--- app/src/main/java/com/ngangavictor/firestore/adapter/ExamAdapter.kt
+++ app/src/main/java/com/ngangavictor/firestore/adapter/ExamAdapter.kt
@@ -1,13 +1,16 @@
 package com.ngangavictor.firestore.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.ngangavictor.firestore.R
 import com.ngangavictor.firestore.holder.ExamHolder
 import com.ngangavictor.firestore.models.ExamModel
 
-class ExamAdapter(private val exams: ArrayList<ExamModel>) :
+class ExamAdapter(private val context: Context,private val exams: ArrayList<ExamModel>) :
     RecyclerView.Adapter<ExamHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExamHolder {
@@ -23,7 +26,33 @@ class ExamAdapter(private val exams: ArrayList<ExamModel>) :
         holder.textViewClass.text = exams[position].examClass
         holder.textViewTerm.text = exams[position].examTerm
         holder.textViewYear.text = exams[position].examYear
-        holder.imageViewMore.setOnClickListener { }
+        holder.imageViewMore.setOnClickListener {
+            val popupMenu = PopupMenu(context, holder.imageViewMore)
+            popupMenu.inflate(R.menu.exam_menu)
+            popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                override fun onMenuItemClick(item: MenuItem?): Boolean {
+                    when (item!!.itemId) {
+                        R.id.action_edit_exam -> {
+//                            listenerDeleteClass.className(classes[position].className)
+                            return true
+                        }
+                        R.id.action_view_exam_results -> {
+                            return true
+                        }
+                        R.id.action_enter_exam_results->{
+
+                        }
+                        R.id.action_delete_exam->{
+
+                        }
+                    }
+                    return false
+                }
+
+            })
+            popupMenu.show()
+
+        }
     }
 
     override fun getItemCount(): Int {
