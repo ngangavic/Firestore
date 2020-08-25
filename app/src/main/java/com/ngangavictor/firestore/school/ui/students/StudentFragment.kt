d6cdad99bb8b101aee7abaef18bd75d763c56665
ngangavic <ngangavictor10@gmail.com>
2020-08-25 17:14:59 +0300

    fragment student class spinner setup

--- app/src/main/java/com/ngangavictor/firestore/school/ui/students/StudentFragment.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/students/StudentFragment.kt
@@ -1,14 +1,21 @@
 package com.ngangavictor.firestore.school.ui.students
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.ngangavictor.firestore.R
 
 class StudentFragment : Fragment() {
@@ -27,6 +34,15 @@ class StudentFragment : Fragment() {
 
     private lateinit var viewModel: StudentViewModel
 
+    private lateinit var classList: List<String>
+
+    private lateinit var spinnerAdapter: ArrayAdapter<String>
+
+    private lateinit var alert: AlertDialog
+
+    private lateinit var database: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,6 +53,24 @@ class StudentFragment : Fragment() {
         recyclerViewStudents=root.findViewById(R.id.recyclerViewStudents)
         spinnerClass=root.findViewById(R.id.spinnerClass)
 
+        database = Firebase.firestore
+        auth = Firebase.auth
+
+        classList = ArrayList()
+
+        spinnerAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, classList)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerClass.adapter = spinnerAdapter
+
+        database.collection("classes").document(auth.currentUser!!.uid)
+            .addSnapshotListener { value, error ->
+                for (i in value!!.data!!.values) {
+                    (classList as ArrayList<String>).add(i.toString())
+                }
+
+                spinnerAdapter.notifyDataSetChanged()
+            }
 
         return root
     }
