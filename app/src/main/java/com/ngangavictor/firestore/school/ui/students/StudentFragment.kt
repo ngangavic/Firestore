4af1da9811c8e0c95ca20536449c7215f9491d7f
ngangavic <ngangavictor10@gmail.com>
2020-08-25 17:50:41 +0300

    permissions request

--- app/src/main/java/com/ngangavictor/firestore/school/ui/students/StudentFragment.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/students/StudentFragment.kt
@@ -1,16 +1,23 @@
 package com.ngangavictor.firestore.school.ui.students
 
+import android.Manifest
 import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -75,18 +82,96 @@ class StudentFragment : Fragment() {
             }
 
         fabAddStudent.setOnClickListener {
-            val addStudentDialog = AddStudentDialog().newInstance()
-            addStudentDialog.isCancelable = false
-            requireActivity().supportFragmentManager.let {
-                addStudentDialog.show(
-                    it,
-                    "dialog add student"
+            if (!checkPermission()) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(), arrayOf(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ), 200
                 )
+            }else{
+                val addStudentDialog = AddStudentDialog().newInstance()
+                addStudentDialog.isCancelable = true
+                requireActivity().supportFragmentManager.let {
+                    addStudentDialog.show(
+                        it,
+                        "dialog add student"
+                    )
+                }
             }
         }
 
         return root
     }
+
+    private fun checkPermission(): Boolean {
+        val resultReadExternalStorage =
+            ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        val resultWriteExternalStorage =
+            ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+
+        return resultReadExternalStorage == PackageManager.PERMISSION_GRANTED && resultWriteExternalStorage == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 200 && grantResults.isNotEmpty()) {
+            val readAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            val writeAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED
+
+            if (!readAccepted && !writeAccepted) {
+                Snackbar.make(
+                    requireView().findViewById(android.R.id.content),
+                    "Storage permission required",
+                    Snackbar.LENGTH_LONG
+                ).show()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        showMessageOKCancel("You need to allow access to both the permissions",
+                            DialogInterface.OnClickListener { dialog, which ->
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                    requestPermissions(
+                                        arrayOf(
+                                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                                        ),
+                                        200
+                                    )
+                                }
+                            })
+                        return
+                    }
+                }
+            } else {
+                Snackbar.make(
+                    requireView().findViewById(android.R.id.content),
+                    "Permissions granted.",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+
+        }
+    }
+
+    private fun showMessageOKCancel(
+        message: String,
+        okListener: DialogInterface.OnClickListener
+    ) {
+        AlertDialog.Builder(requireContext())
+            .setMessage(message)
+            .setPositiveButton("OK", okListener)
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show()
+    }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
