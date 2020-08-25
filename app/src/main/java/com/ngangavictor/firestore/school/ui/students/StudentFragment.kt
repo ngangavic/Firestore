61d9b4f244ca5c028ea6937f616b06c550a5f482
ngangavic <ngangavictor10@gmail.com>
2020-08-25 17:00:04 +0300

    fragment student UI

--- app/src/main/java/com/ngangavictor/firestore/school/ui/students/StudentFragment.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/students/StudentFragment.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.ngangavictor.firestore.R
 
 class StudentFragment : Fragment() {
+
+    lateinit var fabAddStudent:FloatingActionButton
+
+    lateinit var root:View
+
+    lateinit var recyclerViewStudents:RecyclerView
+
+    lateinit var spinnerClass:Spinner
 
     companion object {
         fun newInstance() = StudentFragment()
@@ -20,7 +31,14 @@ class StudentFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.student_fragment, container, false)
+        root=inflater.inflate(R.layout.student_fragment, container, false)
+
+        fabAddStudent=root.findViewById(R.id.fabAddStudent)
+        recyclerViewStudents=root.findViewById(R.id.recyclerViewStudents)
+        spinnerClass=root.findViewById(R.id.spinnerClass)
+
+
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
