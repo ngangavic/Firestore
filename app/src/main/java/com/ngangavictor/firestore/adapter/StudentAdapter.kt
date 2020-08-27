6d2afc111ec47403fc24bd8c3084a79bc8d451f7
ngangavic <ngangavictor10@gmail.com>
2020-08-27 20:50:28 +0300

    student menu setup

--- app/src/main/java/com/ngangavictor/firestore/adapter/StudentAdapter.kt
+++ app/src/main/java/com/ngangavictor/firestore/adapter/StudentAdapter.kt
@@ -2,7 +2,9 @@ package com.ngangavictor.firestore.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.ngangavictor.firestore.R
 import com.ngangavictor.firestore.holder.StudentHolder
@@ -27,13 +29,33 @@ class StudentAdapter(
         holder.textViewParentNo.text = students[position].parentNo
         holder.textViewKCPE.text = students[position].kcpe
         holder.imageViewMore.setOnClickListener {
+            val popupMenu = PopupMenu(context, holder.imageViewMore)
+            popupMenu.inflate(R.menu.student_menu)
+            popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
+                override fun onMenuItemClick(item: MenuItem?): Boolean {
+                    when (item!!.itemId) {
+                        R.id.action_edit_student -> {
+                            return true
+                        }
+                        R.id.action_delete_student -> {
+                            return true
+                        }
+                        R.id.action_view_results_student -> {
+                            return true
+                        }
+                        R.id.action_sms_parent_student->{
+                            return true
+                        }
+                    }
+                    return false
+                }
 
+            })
+            popupMenu.show()
         }
     }
 
     override fun getItemCount(): Int {
         return students.size
     }
-
-
 }
