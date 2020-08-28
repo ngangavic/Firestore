fa28ab09f8edafe2ab9442a34f71844eea0a9ef7
ngangavic <ngangavictor10@gmail.com>
2020-08-28 15:28:25 +0300

    teacher fragment UI

--- app/src/main/java/com/ngangavictor/firestore/school/ui/teachers/TeacherFragment.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/teachers/TeacherFragment.kt
@@ -6,27 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.ngangavictor.firestore.R
 
 class TeacherFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = TeacherFragment()
-    }
-
     private lateinit var viewModel: TeacherViewModel
+
+    private lateinit var root:View
+
+    private lateinit var recyclerViewTeachers:RecyclerView
+
+    private lateinit var fabAddTeacher:FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.teacher_fragment, container, false)
-    }
+        root= inflater.inflate(R.layout.teacher_fragment, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        recyclerViewTeachers=root.findViewById(R.id.recyclerViewTeachers)
+
+        fabAddTeacher=root.findViewById(R.id.fabAddTeacher)
+
         viewModel = ViewModelProviders.of(this).get(TeacherViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        return root
     }
 
 }
