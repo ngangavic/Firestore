8a3166ab0f8910912d5e99d3c9ca5882aa9c21fe
ngangavic <ngangavictor10@gmail.com>
2020-09-01 09:11:16 +0300

    save results

--- app/src/main/java/com/ngangavictor/firestore/adapter/ResultAdapter.kt
+++ app/src/main/java/com/ngangavictor/firestore/adapter/ResultAdapter.kt
@@ -1,18 +1,28 @@
 package com.ngangavictor.firestore.adapter
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.ngangavictor.firestore.R
 import com.ngangavictor.firestore.holder.ResultHolder
+import com.ngangavictor.firestore.listeners.ListenerResult
 import com.ngangavictor.firestore.models.ResultModel
 
 class ResultAdapter(
     private val context: Context,
-    private val results: ArrayList<ResultModel>
-) :
+    private val results: ArrayList<ResultModel>,
+private val listenerResult: ListenerResult) :
     RecyclerView.Adapter<ResultHolder>() {
+
+    private var database:FirebaseFirestore=Firebase.firestore
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultHolder {
         val viewHolder: ResultHolder?
         val layoutView =
@@ -24,6 +34,26 @@ class ResultAdapter(
     override fun onBindViewHolder(holder: ResultHolder, position: Int) {
         holder.textViewAdm.text = "Adm: " + results[position].adm
         holder.editTextMarks.setText(results[position].marks)
+
+        holder.editTextMarks.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                if (count>2){
+                    //save
+                    listenerResult.saveMarks(holder.editTextMarks.text.toString().toInt(),results[position].adm)
+//                }else{
+//                    listenerResult.error("Error: Input two characters")
+//                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/ngangavictor/firestore/school/ui/results/ResultFragment.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/results/ResultFragment.kt
@@ -10,11 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.ngangavictor.firestore.R
 import com.ngangavictor.firestore.adapter.ResultAdapter
 import com.ngangavictor.firestore.dialogs.AddResultsDialog
 import com.ngangavictor.firestore.listeners.ListenerResult
 import com.ngangavictor.firestore.models.ResultModel
+import com.ngangavictor.firestore.utils.LocalSharedPreferences
 
 class ResultFragment : Fragment(), ListenerResult {
 
@@ -28,6 +37,14 @@ class ResultFragment : Fragment(), ListenerResult {
 
     private lateinit var resultList: MutableList<ResultModel>
 
+    private lateinit var fabSaveResults:FloatingActionButton
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var database:FirebaseFirestore
+
+    private lateinit var localSharedPreferences: LocalSharedPreferences
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,8 +55,12 @@ class ResultFragment : Fragment(), ListenerResult {
         viewModel = ViewModelProviders.of(this).get(ResultViewModel::class.java)
 
         recyclerViewResults = root.findViewById(R.id.recyclerViewResults)
+        fabSaveResults = root.findViewById(R.id.fabSaveResults)
 
         resultList = ArrayList()
+
+        auth=Firebase.auth
+        database=Firebase.firestore
 
         recyclerViewResults.layoutManager = LinearLayoutManager(requireContext())
         recyclerViewResults.setHasFixedSize(true)
@@ -53,6 +74,12 @@ class ResultFragment : Fragment(), ListenerResult {
             )
         }
 
+        fabSaveResults.setOnClickListener {
+
+        }
+
+        localSharedPreferences= LocalSharedPreferences(requireContext())
+
         return root
     }
 
@@ -62,7 +89,7 @@ class ResultFragment : Fragment(), ListenerResult {
             resultList = it as MutableList<ResultModel>
 
             resultAdapter = ResultAdapter(
-                requireContext(), resultList as ArrayList<ResultModel>
+                requireContext(), resultList as ArrayList<ResultModel>,this
             )
 
             resultAdapter.notifyDataSetChanged()
@@ -70,6 +97,19 @@ class ResultFragment : Fragment(), ListenerResult {
             recyclerViewResults.adapter = resultAdapter
         })
 
+    }
+
+    override fun saveMarks(marks: Int, adm: String) {
+        val newMarks= hashMapOf(
+            "marks" to marks
+        )
+        database.collection("schools").document(auth.currentUser!!.uid)
+            .collection("results").document(localSharedPreferences.getSelectedExamPref("examKey").toString()).collection(localSharedPreferences.getSelectedExamPref("examSubject").toString()).document(adm).set(newMarks,
+                SetOptions.merge())
+    }
+
+    override fun error(message: String) {
+        Snackbar.make(requireView(),message,Snackbar.LENGTH_LONG).show()
     }
 
 }
