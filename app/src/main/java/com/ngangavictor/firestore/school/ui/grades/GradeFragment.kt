9a7d4429049cbeb9cc9de09eea5605b88892c5fc
ngangavic <ngangavictor10@gmail.com>
2020-09-02 16:03:54 +0300

    grade frag UI

--- app/src/main/java/com/ngangavictor/firestore/school/ui/grades/GradeFragment.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/grades/GradeFragment.kt
@@ -4,29 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.ngangavictor.firestore.R
+import com.ngangavictor.firestore.school.ui.exams.ExamViewModel
 
 class GradeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GradeFragment()
-    }
+   private lateinit var root:View
 
-    private lateinit var viewModel: GradeViewModel
+    private lateinit var gradeViewModel: GradeViewModel
+
+    private lateinit var spinnerClass:Spinner
+
+    private lateinit var recyclerViewGrade:RecyclerView
+
+    private lateinit var fabEditGrade:FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.grade_fragment, container, false)
-    }
+        root= inflater.inflate(R.layout.grade_fragment, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(GradeViewModel::class.java)
-        // TODO: Use the ViewModel
+        gradeViewModel =
+            ViewModelProviders.of(this).get(GradeViewModel::class.java)
+
+        spinnerClass=root.findViewById(R.id.spinnerClass)
+
+        recyclerViewGrade=root.findViewById(R.id.recyclerViewGrade)
+
+        fabEditGrade=root.findViewById(R.id.fabEditGrade)
+
+        return root
     }
 
 }
