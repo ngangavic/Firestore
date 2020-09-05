02b4f863c84ed1ef974a9a96cd5d26527f87990f
ngangavic <ngangavictor10@gmail.com>
2020-09-05 17:17:19 +0300

    clean up

--- app/src/main/java/com/ngangavictor/firestore/school/ui/results/ResultViewModel.kt
+++ app/src/main/java/com/ngangavictor/firestore/school/ui/results/ResultViewModel.kt
@@ -1,6 +1,5 @@
 package com.ngangavictor.firestore.school.ui.results
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ngangavictor.firestore.models.ResultModel
@@ -15,11 +14,9 @@ class ResultViewModel : ViewModel() {
     private val resultData = MutableLiveData<List<ResultModel>>()
 
     fun getResultData(subjectName: String, examId: String): MutableLiveData<List<ResultModel>> {
-
         firestoreRepository.getResults(examId, subjectName).addSnapshotListener { value, error ->
             resultList.clear()
             for (i in value!!) {
-                Log.e("RESULTS", i.id + " " + i.data["marks"].toString())
                 resultList.add(
                     ResultModel(
                         i.id,
